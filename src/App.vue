c4f4fa658efa67f47ea03d49bdfff4ef6fcc7624
unknown <941251418@qq.com>
2020-04-28 11:54:20 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
-    <div id="app">
-       
-    </div>
+  <div id="app">
+      <router-view></router-view>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'App',
-        components: {
-            
-        }
-    }
+export default {
+  name: "App",
+  components: {}
+};
 </script>
 
 <style>
-
 </style>
